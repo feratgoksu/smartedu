0c32eb4d8fef796dd8b2e578911c2e0d393e8819
ferat.goksu <ferat.goksu@ibb.gov.tr>
2023-12-07 13:29:14 +0300

    smartedu1

--- app.js
+++ app.js
@@ -2,12 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('index')
-})
-
 const port = 3000;
 
 app.listen(port,()=>{
     console.log(`App started on port ${port}`);
 })
+//TEMPLATE ENGİNE
+
+app.set("view engine","ejs");
+
+//MIDDLEWARES
+
+app.use(express.static("public"));
+
+//ROUTES
+app.get('/',(req,res)=>{
+    res.render('index',{
+        page_name: "index"
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        page_name: "about"
+    })
+})
+
+
